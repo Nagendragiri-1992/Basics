83cf90e61427e1eec2263560a3d7248898ab9cc8
Nagendragiri N <nagendragiri1992@outlook.com>
2025-03-31 19:47:02 +0530

    Interface Upload

---
+++ Interface/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+interface IShape
+{
+    void Draw();  // Method declaration
+}
+
+interface IColor
+{
+    void Paint(string color);  // Another interface
+}
+
+class Circle : IShape, IColor
+{
+    public void Draw() => Console.WriteLine("Drawing a Circle.");
+    public void Paint(string color) => Console.WriteLine($"Circle painted with {color} color.");
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Circle circle = new Circle();
+        circle.Draw();
+        circle.Paint("Blue");
+    }
+}
